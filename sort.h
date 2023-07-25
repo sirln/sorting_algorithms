847cf6d34cac7f1f1f068b2e88d343ab7e1e9eb3
sirln <siro@sirlawren.com>
2023-07-26 00:14:33 +0300

    Final

--- sort.h
+++ sort.h
@@ -17,6 +17,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
 /* printing functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
